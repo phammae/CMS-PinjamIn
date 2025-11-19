b8d11b7d0e1375503db0a6c827fd73091ba287a7
phammae <mamrizal823@gmail.com>
2025-11-19 15:27:59 +0700

    Initial setup: Vite + React + TS + Tailwind + shadcn + API client

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div className='flex min-h-svh flex-col items-center justify-center'>
+          <Button>Click Me</Button>
+        </div>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
