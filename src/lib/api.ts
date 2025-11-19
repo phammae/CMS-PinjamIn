20eb1bee8de88d21154bf21534883983a9e0b978
phammae <mamrizal823@gmail.com>
2025-11-19 15:48:30 +0700

    Fix setup commit

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,29 +1,31 @@
 import axios from "axios";
 
-const api = axios.create ({
-    baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: false,
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: false,
 });
 
+// REQUEST INTERCEPTOR
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Autorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
+  return config;
 });
 
+// RESPONSE INTERCEPTOR
 api.interceptors.response.use(
-    (res) => res,
-    (err) => {
-        if (err.response?.status === 401) {
-            localStorage.removeItem("token");
-            window.location.href = "/login";
-        }
-        return Promise.reject(err);
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
     }
+    return Promise.reject(err);
+  }
 );
 
 export default api;
